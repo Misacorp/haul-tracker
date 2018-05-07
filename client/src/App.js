50d75dd7bc8d476fbb2cb2843f43a96872df56c3
Misa Jokisalo <misa.jokisalo@gmail.com>
2018-05-07 17:44:33 +0300

    Implemented routes with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-import HaulInput from './HaulInput';
+import Routes from './Routes';
 
 const styles = {
   main: {
@@ -11,14 +15,53 @@ const styles = {
   },
 };
 
-function App() {
-  return (
-    <div style={styles.main} >
-      <MuiThemeProvider>
-        <HaulInput />
-      </MuiThemeProvider>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { drawerOpen: false };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ drawerOpen: !this.state.drawerOpen });
+  }
+
+  handleClose() {
+    this.setState({ drawerOpen: false });
+  }
+
+  render() {
+    return (
+      <div style={styles.main} >
+        <MuiThemeProvider>
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.drawerOpen}
+            onRequestChange={drawerOpen => this.setState({ drawerOpen })}
+          >
+            <Link to="/">
+              <MenuItem to="/" onClick={this.handleClose}>Login</MenuItem>
+            </Link>
+            <Link to="/newhaul">
+              <MenuItem onClick={this.handleClose}>New haul</MenuItem>
+            </Link>
+          </Drawer>
+
+          <AppBar
+            title="Haul Tracker"
+            onClick={this.handleToggle}
+          />
+
+          <div style={styles.main}>
+            <Routes />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
